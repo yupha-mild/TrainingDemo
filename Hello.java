0563d3ee635eda646e075a8d05b74f5fd6eef50a
Krerk Piromsopa <pkrerk@gmail.com>
2017-08-31 15:54:55 +0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+class Hello {
+    
+    public Hello() {
+        System.out.println("Hello! We are:");
+        this.Krerk();
+    }
+    
+    private void Krerk() {
+        System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+    }
+    
+    public static void main(String [] argv) {
+        new Hello();
+    }
+}
