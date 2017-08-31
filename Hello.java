a00d1ffeea0b858303b7ba8e96706f2ee03ef954
D:\11_GIT_SourceTree <wiwekwaew.mild@gmail.com>
2017-08-31 19:15:34 +0700

    Commit Again #2

--- Hello.java
+++ Hello.java
@@ -2,14 +2,16 @@ class Hello {
     
     public Hello() {
         System.out.println("Hello! We are:");
-        this.Krerk();
+	this.Krerk();
+		this.Mild();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
-        System.out.println("Trainee - Yuphaphorn Wiwekwaew --- ");
     }
-    
+    private void Mild() {
+        System.out.println("Trainee - Yuphaphorn Wiwekwaew");
+    }
     public static void main(String [] argv) {
         new Hello();
     }
